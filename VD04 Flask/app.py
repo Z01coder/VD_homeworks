84dcbb75b77edcff941f698139b8549c63a4febe
xann <ramatahatta.itsari@gmail.com>
2024-11-11 14:47:17 +0400

    Completed lesson assignment VD06 added

--- VD04 Flask/app.py
+++ VD04 Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 import locale
 
@@ -9,19 +9,45 @@ try:
 except locale.Error:
     print("Ошибка")
 
+# Хранилище анкет
+user_data = []
+
+
 @app.route('/')
 def index():
     now = datetime.now()
     formatted_date = now.strftime("Сегодня %d %B %Y, сейчас %H:%M")
     return render_template('index.html', date_time=formatted_date)
 
-@app.route('/blog')
+
+@app.route('/blog', methods=['GET', 'POST'])
 def blog():
-    return render_template('blog.html')
+    global user_data
+    if request.method == 'POST':
+        # Достаем данные из формы
+        name = request.form.get('name')
+        city = request.form.get('city')
+        hobby = request.form.get('hobby')
+        age = request.form.get('age')
+
+        # Пополняем хранилище анкет
+        user_data.append({
+            'name': name,
+            'city': city,
+            'hobby': hobby,
+            'age': age
+        })
+
+        # Редирект
+        return redirect(url_for('blog'))
+
+    return render_template('blog.html', user_data=user_data)
+
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
